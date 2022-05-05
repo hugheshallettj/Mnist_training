0d254c62a12ed3639c3ad8b1f12e677f2e7d32a4
Jacob Hughes-Hallett <hh.jacob@gmail.com>
2022-05-05 15:51:24 +0100

    tidied up code so that the entire thing can be run

--- Mnist_training.py
+++ Mnist_training.py
@@ -1,6 +1,7 @@
-# %% Load Libraries
-# -- Load Libraries
 
+# %% Load Libraries
+
+from threading import local
 import gpflow
 import tensorflow as tf
 import numpy as np
@@ -8,7 +9,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import sklearn 
 from sklearn.utils.multiclass import unique_labels
-from sklearn.datasets import load_iris, load_diabetes, load_breast_cancer, load_digits
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics import accuracy_score
@@ -55,6 +55,8 @@ def load_mnist(y_hot_encoding=True):
     # preprocessing
     x_train = x_train.reshape(60000, 784)/255
     x_test = x_test.reshape(10000, 784)/255
+    y_train = y_train.reshape(-1, 1)
+    y_test = y_test.reshape(-1, 1)
 
     if y_hot_encoding==True:
         C = len(unique_labels(y_train))
@@ -66,15 +68,20 @@ def load_mnist(y_hot_encoding=True):
         return x_train, x_test, y_train, y_test
 
 
-# %%
+# %% Make function for training Sparse models on MNIST dataset
 
 def train_mnist(
     model,
     num_ind_points,
     num_iterations,
+    inducing_point_locations="Random",
     train_ind_points=True,
+    likelihood_func="Gaussian",
     learning_rate_adam=0.1,
     minibatch_size=None,
+    initial_kernel_variance=1,
+    initial_kernel_lengthscale=10,
+    initial_likelihood_hyperparameter=1
     ):
     """Trains sparse GP models on MNIST dataset.
 
@@ -83,6 +90,7 @@ def train_mnist(
         num_ind_points (int): Number of inducing points to be used in the model.
         num_iterations (int): Number of iterations during optimisation.
         train_ind_points (bool, optional): Set inducing points to be a trainable parameter. Defaults to True.
+        likelihood_func (str, optional): Likelihood function to use for GP model. Defaults to Gaussian ("RobustMax" also available).
         learning_rate_adam (float, optional): Learning rate to use in Adam optimiser. Defaults to 0.01.
         minibatch_size (int, optional): Size of minibatch to use during optimisation. Defaults to None (full batch training).
 
@@ -102,9 +110,12 @@ def train_mnist(
     if not type(num_iterations) is int:
         raise ValueError("Iterations must be an integer.")
     if minibatch_size is not None and not type(minibatch_size) is int:
-            raise ValueError("Minibatch size must be an integer.")
+        raise ValueError("Minibatch size must be an integer.")
+    if type(inducing_point_locations) != str and not len(inducing_point_locations)==num_ind_points:
+        raise ValueError("Number of inducing points must match length of inducing locations array")
 
-    y_hot_encoding = model=="SGPR"
+
+    y_hot_encoding = likelihood_func!="RobustMax"
 
     x_train, x_test, y_train, y_test = load_mnist( y_hot_encoding=y_hot_encoding)
 
@@ -115,15 +126,21 @@ def train_mnist(
     #* Number of inducing points
     M = num_ind_points
 
-    #* Random initialisation of inducing points
-    Z = x_train[:M, :].copy()
+    #* Initialisation of inducing points
+    if inducing_point_locations=="Random":
+        Z = x_train[:M, :].copy()
+    else:
+        Z = inducing_point_locations
 
     #* Number of classes for a classification dataset
     C = len(unique_labels(y_train))
 
-    #* Robustmax Multiclass Likelihood
-    invlink = gpflow.likelihoods.RobustMax(C)  # Robustmax inverse link function
-    likelihood = gpflow.likelihoods.MultiClass(C, invlink=invlink)  # Multiclass likelihood
+    #* Likelihood
+    if likelihood_func=="RobustMax":
+        invlink = gpflow.likelihoods.RobustMax(C)  # Robustmax inverse link function
+        likelihood = gpflow.likelihoods.MultiClass(C, invlink=invlink)  # Multiclass likelihood
+    elif likelihood_func=="Gaussian":
+        likelihood = gpflow.likelihoods.Gaussian()
 
     #* Kernel
     kernel = gpflow.kernels.SquaredExponential()
@@ -136,7 +153,7 @@ def train_mnist(
             num_latent_gps=C,
             likelihood=likelihood,
             )
-    if model=="SGPR":
+    elif model=="SGPR":
         m = gpflow.models.SGPR(
             data=data,
             inducing_variable=Z,
@@ -147,18 +164,20 @@ def train_mnist(
     jitter = 1e-3
     gpflow.config.set_default_jitter(jitter)
 
+    #* Initialise hyperparams
+    if likelihood_func=="Gaussian":
+        m.likelihood.variance.assign(initial_likelihood_hyperparameter)
+    # if likelihood_func=="RobustMax":
+    #     m.likelihood.invlink.epsilon.assign(initial_likelihood_hyperparameter)
+    m.kernel.variance.assign(initial_kernel_variance)
+    m.kernel.lengthscales.assign(initial_kernel_lengthscale)
+
     #* Set trainable parameters
     gpflow.set_trainable(m.inducing_variable, train_ind_points)
+    gpflow.set_trainable(m.kernel, train_ind_points)
+    gpflow.set_trainable(m.likelihood, train_ind_points)
 
-    #* Initialise hyperparams
-    var_y = 1
-    var_f = var_y
-    kappa = 1
-    var_n = var_f / kappa
-
-    # m_SVGP.likelihood.variance.assign(var_n)
-    m.kernel.variance.assign(var_y)
-    m.kernel.lengthscales.assign(10)
+    gpflow.utilities.print_summary(m, fmt="notebook")
 
     #* Optimiser object
     opt = tf.optimizers.Adam(learning_rate=learning_rate_adam)
@@ -194,7 +213,7 @@ def train_mnist(
         accuracy_logs.append(accuracy_score(y_test, y_pred_labels))
         watch.start()
         
-        # logs.append(float(m.log_marginal_likelihood()))
+        #// logs.append(float(m.log_marginal_likelihood()))
 
 
     if model=="SGPR":
@@ -202,7 +221,7 @@ def train_mnist(
 
     if model=="SVGP" and minibatch_size is None:
         training_loss= m.training_loss_closure(data)
-    if model=="SVGP" and minibatch_size is not None:
+    elif model=="SVGP" and minibatch_size is not None:
         train_dataset = tf.data.Dataset.from_tensor_slices((data)).repeat().shuffle(N)
         train_iter = iter(train_dataset.batch(minibatch_size))
         training_loss = m.training_loss_closure(train_iter, compile=True)
@@ -230,18 +249,32 @@ def train_mnist(
     print("Total time for optimisation: {} seconds".format(watch.read_time()))
     gpflow.utilities.print_summary(m, fmt="notebook")
 
-    if model=="SGPR":
+    if likelihood_func=="Gaussian" or model=="SGPR":
         final_hyperparams = {
             "kernel variance": m.kernel.variance.numpy(),
             "kernel lengthscale": m.kernel.lengthscales.numpy(),
             "likelihood variance": m.likelihood.variance.numpy(),
-            }
-    elif model=="SVGP":
+            "inducing locations": m.inducing_variable.Z.numpy(),
+        }
+
+        initial_hyperparams = {
+            "kernel variance": initial_kernel_variance,
+            "kernel lengthscale": initial_kernel_lengthscale,
+            "initial noise variance": initial_likelihood_hyperparameter,
+            "initial inducing locations": inducing_point_locations
+        }
+        
+    elif likelihood=="RobustMax":
         final_hyperparams = {
             "kernel variance": m.kernel.variance.numpy(),
             "kernel lengthscale": m.kernel.lengthscales.numpy(),
             "likelihood invlink epsilon": m.likelihood.invlink.epsilon.numpy(),
             }
+        initial_hyperparams = {
+            "kernel variance": initial_kernel_variance,
+            "kernel lengthscale": initial_kernel_lengthscale,
+            #"initial noise variance": initial_likelihood_hyperparameter,
+        }
 
     return (
         model,
@@ -250,10 +283,13 @@ def train_mnist(
         learning_rate_adam,
         minibatch_size,
         num_iterations,
+        likelihood_func,
         times,
         elbo_logs,
         accuracy_logs,
-        final_hyperparams
+        initial_hyperparams,
+        final_hyperparams,
+        m
         )
 
 
@@ -266,15 +302,86 @@ col_names =  [
     "Adam learning rate",
     "minibatch size",
     "iterations",
+    "likelihood",
     "time logs",
-    "ELBo logs",
+    "ELBO logs",
     "accuracy logs",
-    "final hyperparams"
+    "initial hyperparams",
+    "final hyperparams",
+    "GPflow model object"
     ]
   
 # create an empty dataframe
 # with columns
-my_df  = pd.DataFrame(columns = col_names)
+results_df  = pd.DataFrame(columns = col_names)
   
 # show the dataframe
-my_df
+# results_df
+# %%
+
+results_df.loc[len(results_df.index)] = train_mnist("SGPR", 500, 200)
+
+# %%
+
+var_SVGP = results_df["final hyperparams"].loc[results_df.index[0]]["kernel variance"]
+len_SVGP = results_df["final hyperparams"].loc[results_df.index[0]]["kernel lengthscale"]
+noi_SVGP = results_df["final hyperparams"].loc[results_df.index[0]]["likelihood variance"]
+Z_SVGP = results_df["final hyperparams"].loc[results_df.index[0]]["inducing locations"]
+
+print(var_SVGP, len_SVGP, noi_SVGP)
+
+results_df.loc[len(results_df.index)] = train_mnist(
+    "SVGP",
+    500,
+    5000,
+    "Random",
+    False,
+    "Gaussian",
+    minibatch_size=500,
+    initial_kernel_variance=var_SVGP,
+    initial_kernel_lengthscale=len_SVGP, initial_likelihood_hyperparameter=noi_SVGP,
+)
+
+# %%
+
+# plt.plot(results_df["time logs"].loc[results_df.index[0]], results_df["ELBO logs"].loc[results_df.index[0]])
+# plt.plot(results_df["time logs"].loc[results_df.index[1]], results_df["ELBO logs"].loc[results_df.index[1]])
+# plt.show()
+
+# plt.plot(results_df["time logs"].loc[results_df.index[0]], results_df["accuracy logs"].loc[results_df.index[0]])
+# plt.plot(results_df["time logs"].loc[results_df.index[1]], results_df["accuracy logs"].loc[results_df.index[1]])
+# plt.grid(True, alpha=0.2)
+# plt.show()
+# %%
+results_df.to_csv(
+    "local_SparseGP_MNIST_results.csv"
+    # mode="a",
+    # header=False,
+)
+# %%
+
+# m_sgpr = results_df["GPflow model object"].loc[results_df.index[0]]
+# m_svgp = results_df["GPflow model object"].loc[results_df.index[1]]
+
+# # %%
+# mu, cov = m_sgpr.compute_qu()
+
+# m_svgp
+# # %%
+# m_svgp.q_mu.assign(mu)
+# m_svgp.q_sqrt.assign(cov)
+
+# x_train, x_test, y_train, y_test = load_mnist(y_hot_encoding=False)
+
+# res1=m_svgp.predict_y(x_test)
+
+# res2=m_sgpr.predict_y(x_test)
+
+# print(res1[:20].reshape(-1, 1))
+# print(res2[:20].reshape(-1, 1))
+
+
+# %%
+
+# np.shape(cov)
+# %%
